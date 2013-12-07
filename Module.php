24d339e941d7e3c7a4da14821d5f3fd6323617b8
Matthew Weier O'Phinney <matthew@zend.com>
2013-12-07 15:13:35 -0600

    Relocate Module classfile to top-level directory

--- Module.php
+++ Module.php
@@ -4,9 +4,57 @@
  * @copyright Copyright (c) 2013 Zend Technologies USA Inc. (http://www.zend.com)
  */
 
-/**
- * This file is placed here for compatibility with Zendframework's ModuleManager.
- * It allows usage of this module even without composer.
- * The original Module.php lives in 'src' directory in order to respect PSR-0
- */
-require_once __DIR__ . '/src/ZF/Rpc/Module.php';
+namespace ZF\Rpc;
+
+class Module
+{
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array('namespaces' => array(
+                __NAMESPACE__ => __DIR__ . '/src/ZF/Rpc/',
+            )),
+        );
+    }
+
+    /**
+     * Retrieve module configuration
+     *
+     * @return array
+     */
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function getServiceConfig()
+    {
+        return array('factories' => array(
+            'ZF\Rpc\OptionsListener' => function ($services) {
+                $rpcConfig = array();
+                if ($services->has('Config')) {
+                    $config = $services->get('Config');
+                    if (isset($config['zf-rpc'])) {
+                        $rpcConfig = $config['zf-rpc'];
+                    }
+                }
+                return new OptionsListener($rpcConfig);
+            },
+        ));
+    }
+
+    public function onBootstrap($e)
+    {
+        $app      = $e->getApplication();
+        $services = $app->getServiceManager();
+
+        // Attach OptionsListener
+        $optionsListener = $services->get('ZF\Rpc\OptionsListener');
+        $optionsListener->attach($app->getEventManager());
+
+        // Setup json strategy
+        $strategy = $services->get('ViewJsonStrategy');
+        $view     = $services->get('ViewManager')->getView();
+        $strategy->attach($view->getEventManager());
+    }
+}
