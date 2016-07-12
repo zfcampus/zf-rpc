6862f78aa8751634c0d608d1f971406cea79fd06
Matthew Weier O'Phinney <matthew@zend.com>
2016-07-12 16:40:57 -0500

    Extracted OptionsListener factory to its own class
    - Also added tests for the factory.

--- src/Module.php
+++ src/Module.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
- * @copyright Copyright (c) 2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright Copyright (c) 2014-2016 Zend Technologies USA Inc. (http://www.zend.com)
  */
 
 namespace ZF\Rpc;
@@ -18,29 +18,13 @@ class Module
         return include __DIR__ . '/../config/module.config.php';
     }
 
-    public function getServiceConfig()
-    {
-        return array('factories' => array(
-            'ZF\Rpc\OptionsListener' => function ($services) {
-                $rpcConfig = array();
-                if ($services->has('Config')) {
-                    $config = $services->get('Config');
-                    if (isset($config['zf-rpc'])) {
-                        $rpcConfig = $config['zf-rpc'];
-                    }
-                }
-                return new OptionsListener($rpcConfig);
-            },
-        ));
-    }
-
     public function onBootstrap($e)
     {
         $app      = $e->getApplication();
         $services = $app->getServiceManager();
 
         // Attach OptionsListener
-        $optionsListener = $services->get('ZF\Rpc\OptionsListener');
+        $optionsListener = $services->get(OptionsListener::class);
         $optionsListener->attach($app->getEventManager());
 
         // Setup json strategy
